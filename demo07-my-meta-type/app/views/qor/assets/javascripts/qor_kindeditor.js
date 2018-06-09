12d16afb5e48c8875069869db25c29899378a5de
golangpkg <you@example.com>
2018-06-10 04:56:44 +0800

    fix kindeditor edit not load bug .

--- demo07-my-meta-type/app/views/qor/assets/javascripts/qor_kindeditor.js
+++ demo07-my-meta-type/app/views/qor/assets/javascripts/qor_kindeditor.js
@@ -1,12 +1,52 @@
-$(function($) {
-    var editor = KindEditor.create('#Content',{
-        uploadJson : '/admin/kindeditor/upload',
-        allowFileManager : false,
-        afterBlur: function(){this.sync();}
-    });
-    var uploadEditor = KindEditor.editor({
-        uploadJson : '/admin/kindeditor/upload?dir=image',
-        allowFileManager : false
+
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node / CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals.
+        factory(jQuery);
+    }
+})(function($) {
+    'use strict';
+
+    let componentHandler = window.componentHandler,
+        NAMESPACE = 'qor.kindeditor',
+        EVENT_ENABLE = 'enable.' + NAMESPACE,
+        EVENT_DISABLE = 'disable.' + NAMESPACE,
+        EVENT_UPDATE = 'update.' + NAMESPACE,
+        SELECTOR_COMPONENT = '[class*="mdl-js"],[class*="mdl-tooltip"]';
+
+    function enable(target) {
+        console.log("################### kindeditor enable ###################");
+        /*jshint undef:false */
+        var editor = KindEditor.create('#kindeditor-id',{
+            uploadJson : '/common/kindeditor/upload?dir=image',
+            allowFileManager : false,
+            afterBlur: function(){this.sync();}
+        });
+    }
+
+    function disable(target) {
+        console.log("################### kindeditor disable ###################");
+    }
+
+    $(function() {
+        $(document)
+            .on(EVENT_ENABLE, function(e) {
+                enable(e.target);
+            })
+            .on(EVENT_DISABLE, function(e) {
+                disable(e.target);
+            })
+            .on(EVENT_UPDATE, function(e) {
+                disable(e.target);
+                enable(e.target);
+            });
     });
 });
+
 console.log("################### kindeditor load finish ###################");
--- demo07-my-meta-type/main.go
+++ demo07-my-meta-type/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 	"github.com/qor/slug"
 	"github.com/astaxie/beego"
+	"github.com/qor/media/asset_manager"
 )
 
 // 用户
@@ -27,7 +28,7 @@ type Product struct {
 func main() {
 	// 注册数据库，可以是sqlite3 也可以是 mysql 换下驱动就可以了。
 	DB, _ := gorm.Open("sqlite3", "demo.db")
-	DB.AutoMigrate(&Product{}, ) //自动创建表。
+	DB.AutoMigrate(&Product{}, &asset_manager.AssetManager{},) //自动创建表。
 
 	// 初始化admin 还有其他的，比如API
 	Admin := admin.New(&admin.AdminConfig{SiteName: "demo", DB: DB})
@@ -39,6 +40,17 @@ func main() {
 	product.Meta(&admin.Meta{Name: "Password", Type: "password"})
 	//product.Meta(&admin.Meta{Name: "Description", Type: "rich_editor"})
 
+	assetManager := Admin.AddResource(&asset_manager.AssetManager{}, &admin.Config{Invisible: true})
+	product.Meta(&admin.Meta{Name: "Description", Config: &admin.RichEditorConfig{
+		AssetManager: assetManager,
+		Plugins:      []admin.RedactorPlugin{
+		  {Name: "medialibrary", Source: "/admin/assets/javascripts/qor_redactor_medialibrary.js"},
+		  {Name: "table", Source: "/admin/assets/javascripts/qor_kindeditor.js"},
+		},
+		Settings: map[string]interface{}{
+		  "medialibraryUrl": "/admin/product_images",
+		},
+	  }})
 	//https://doc.getqor.com/admin/extend_admin.html#create-new-meta-types
 	//自定义组件：
 	product.Meta(&admin.Meta{Name: "Description2", Type: "kindeditor"})
